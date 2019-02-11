0f97b293585266b6957bc30fc469b56b7f3dc8ff
RyanWolfen7 <wolfen7lada@gmail.com>
2019-02-11 13:34:17 +0000

    added on click for poke dex

--- src/component/pokedex.js
+++ src/component/pokedex.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import pokedex from '../resource/pokedek-closed.png';
+import pokedexOpen from '../resource/pokedek-opened.png';
 
 class Pokedex extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokedex: <img src={pokedex} alt='closed pokedex'/>
+    };
+  }
+
+  handleClick = () => {
+    this.setState({pokedex: <img src={pokedexOpen} alt='open pokedex'/>})
+  }
 
   render () {
     return (
       <div className='pokedexContainer'>
-        <img src={pokedex} alt='closed pokedex'/>
+        {this.state.pokedex}
+        <div>
+          <button onClick={this.handleClick}> Open </button>
+        </div>
       </div>
     )
   }
