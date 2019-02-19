fb722536dc79770c9d814595b16ed804e05203f2
Marius Guei <marius.g.guei@gmail.com>
2019-02-19 11:09:21 +0000

    making fetch call to api and storing data as json

--- src/component/search.js
+++ src/component/search.js
@@ -1,12 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Names from './components/names';
+import Form from './components/form';
+import Pokemon from './components/pokemon';
 
 
 class Search extends Component {
+  state = {
+  id: undefined,
+  name: undefined,
+  weight: undefined,
+  error: undefined
+}
+
+  getPokemon = async (e) => {
+
+e.preventDefault();
+
+const name = e.target.elements.pokemon.value;
 
 
-  render() {
+  const api_call = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+
+  const response = await api_call.json();
+
+  console.log(response);
+
+
+}
+
+   render() {
     return (
+     <div>
+       <header>
+         <Names />
+       </header>
+       <section>
+         <Form loadPokemon={this.getPokemon}/>
+       </section>
+       <main>
+         <Pokemon />
+       </main>
 
+
+     </div>
     )
   }
 }
