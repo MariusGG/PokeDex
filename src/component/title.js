bb8f54c96f5fe4b2283d87344348fcd882501cb9
Marius Guei <marius.g.guei@gmail.com>
2019-02-19 11:43:11 +0000

    added a title component

---
+++ src/component/title.js
@@ -0,0 +1,15 @@
+import React, from 'react';
+
+class Title extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1> Pokemon App </h1>
+        <p> Helps you find your favourite Pokemon... </p>
+      </div>
+    );
+  }
+
+}
+export default Title;
